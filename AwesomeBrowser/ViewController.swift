b19a696147875d2f99f18c06b8a2d46fe83997a0
gorgrigoryan <gorgrigoryan@gorgrigoryans-iMac.local>
2019-10-21 11:31:34 -0700

    adding webView on storyboard file

---
+++ AwesomeBrowser/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AwesomeBrowser
+//
+//  Created by Gor Grigoryan on 10/21/19.
+//  Copyright © 2019 Gor Grigoryan. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
